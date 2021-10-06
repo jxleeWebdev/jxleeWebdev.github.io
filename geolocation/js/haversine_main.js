acf8a3ec79395fcb05921b2080b6b4febabf3f20
Lee Ji Xuan <jixuan.lee@gmail.com>
2021-10-06 17:39:37 +0800

    added updated

--- geolocation/js/haversine_main.js
+++ geolocation/js/haversine_main.js
@@ -9,15 +9,15 @@ function calultiondistance(lat,lon){
 let elLocate = document.getElementById("locate")
 elLocate.addEventListener("click",function()){
     if(navigator.geolocation){
-         navigator.geolocation.getcurrentPosition(function(getCurrentPosition){
+         navigator.geolocation.getCurrentPosition(function(position){
             let elLat = document.getElementById("lat") 
             let elLong = document.getElementById("long")
             let elDpulze = document.getElementById("depulze")
             let elKlcc = document.getElementById("klcc")
-            let elsunway = document.getElementById("sunway")
+            let elSunway = document.getElementById("sunway")
 
-            let userlat = position.coords.latitude
-            let uselong = position.coords.longitude
+            let userLat = position.coords.latitude
+            let useLong = position.coords.longitude
 
             let distance = calculateDistance(userLat, userLong)
 
@@ -25,7 +25,7 @@ elLocate.addEventListener("click",function()){
             elLong.innerText = "Your longitude" + userLong
             elDpulze.innerHTML = "Distance to Dpulze , Cyberjaya is" + distance[0] + "km"
             elKlcc.innerHTML = "Distance to KLCC"  + distance[0] + "km"
-            elsunway.innerHTML = "Distance to Sunway Pyramid is "  + distance[0] + "km"
+            elSunway.innerHTML = "Distance to Sunway Pyramid is "  + distance[0] + "km"
          })
          
 
