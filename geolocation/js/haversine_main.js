f379001612615c363f615ba6ffeeff6805ef130e
Lee Ji Xuan <jixuan.lee@gmail.com>
2021-10-06 17:46:46 +0800

    added

--- geolocation/js/haversine_main.js
+++ geolocation/js/haversine_main.js
@@ -1,13 +1,13 @@
 function calultiondistance(lat,lon){
     let dpluze = haversine(lat,lon,2.922561,101.650965)  
     let pyramid= haversine(lat,lon,3.073065,101.607787)       
-    let klcc = haversine(lat,lon3.158761,101.714524)
+    let klcc = haversine(lat,lon,3.158761,101.714524)
 
     return[dpluze,pyramid,klcc]
 }
 
 let elLocate = document.getElementById("locate")
-elLocate.addEventListener("click",function()){
+elLocate.addEventListener("click",function() {
     if(navigator.geolocation){
          navigator.geolocation.getCurrentPosition(function(position){
             let elLat = document.getElementById("lat") 
@@ -26,10 +26,8 @@ elLocate.addEventListener("click",function()){
             elDpulze.innerHTML = "Distance to Dpulze , Cyberjaya is" + distance[0] + "km"
             elKlcc.innerHTML = "Distance to KLCC"  + distance[0] + "km"
             elSunway.innerHTML = "Distance to Sunway Pyramid is "  + distance[0] + "km"
-         })
-         
-
+         })       
     }else {
         alert("Geolocation is not support")
     }
-}
+})
