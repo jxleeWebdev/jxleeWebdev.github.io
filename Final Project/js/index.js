751c28563d4f87491b3ac156d6145af51e946235
Lee Ji Xuan <jixuan.lee@gmail.com>
2021-10-27 15:28:42 +0800

    this is my final project

---
+++ Final Project/js/index.js
@@ -0,0 +1,11 @@
+function promotion1() {
+    document.getElementById("promo1").innerHTML = "You will have you 10% off on your next tour booking!";
+  }
+
+  function promotion2() {
+    document.getElementById("promo2").innerHTML = "You will have you 20% off on your next tour booking!";
+  }
+  
+  function promotion3() {
+    document.getElementById("promo3").innerHTML = "You will have you 30% off on your next tour booking + one free meal deal!";
+  }
